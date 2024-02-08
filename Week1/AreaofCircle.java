8280f5851ff8f3c7fac974dddc72bf19ecac3612
pairupwithpinal <emailpinalpatel@gmail.com>
2024-02-08 11:16:14 -0500

    Week1

---
+++ Week1/AreaofCircle.java
@@ -0,0 +1,14 @@
+package Week1;
+
+public class AreaofCircle {
+    public static void main(String[] args) {
+        // Declare radius
+        double radius = 12.4;
+        // Declare pi
+        double PI = 3.14;
+        // calculate area
+        double area = radius * radius * PI;
+        //print
+        System.out.println("Area of the circle is: "+ area);
+    }
+}
---
+++ Week1/Structure.java
@@ -0,0 +1,9 @@
+package Week1;
+
+public class Structure {
+
+
+    public static void main(String[] args) {
+        System.out.println("Learning about code structure in java.");
+    }
+}
